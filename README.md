769923cffed56fed10f59cc0627a1bb505de4c4e
Carrie Young <carrieky04@gmail.com>
2022-12-20 18:10:33 -0800

    adds working application, adds some readme

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # Employee-Tracker
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+The goal of this project was to create an employee tracker for business owners. Through this application owners are able to view their departments, it's roles, and the employees for each role, keeping their business organized.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Credits](#credits)
+- [Questions](#questions)
+- [License](#license)
+
+
+## Installation
+
+If you have a MAC, use Homebrew to install node. From your terminal, insert this command line:
+
+```
+ brew install node
+ ```
+
+If you have Windows, use Winget to install node. From your command line, insert this command: 
+
+```
+winget install OpenJS.NodeJS
+```
+Once node is installed, you will need install a package.json. Run the following in your CLI:
+
+```
+nmp init -y
+```
+Once you have node and package.json installed, then you need to install the mysql to connect to your database information. Insert this in your CLI:
+
+```
+npm install --save mysql2
+```
+
+
+## Usage
+
+You can find this application deployed on Heroku:
+
+[Heroku](https://carrieky-note-taker.herokuapp.com/)
+
+To run this application, you will need to start the server by using this command in your CLI:
+
+```
+node index.js
+```
+
+![Homepage](./public/assets/images/Homepage.png)
+
+![Notepage](./public/assets/images/Notepage.png)
+
+## Tests
+
+No testing required
+
+## Credits
+
+None
+
+## Questions
+
+[Github](https://github.com/carrieky04)
+
+[Contact](mailto:carrieky04@gmail.com)
+
+## License
+
+MIT
+
+Copyright (c) [2022] [Carrie-Young]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,222 +1,327 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const cTable = require('console.table');
-
-
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
 
 // Create database connection
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'PassWord(1216)',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-  );
-
-
-
+  {
+    host: "localhost",
+    user: "root",
+    password: "PassWord(1216)",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
 
 // Inquirer Question Objects
 const menu = [
-    {
-        type: 'list',
-        name: 'menu',
-        message: 'Choose from the options below: ',
-        choices: ['View All Departments', 
-        'View All Roles', 'View All Employees', 
-        'Add a Department', 'Add a Role', 
-        'Add an Employee', 'Update an Employee Role', 'Done'],
-    }
-]
+  {
+    type: "list",
+    name: "menu",
+    message: "Choose from the options below: ",
+    choices: [
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update an Employee Role",
+      "Done",
+    ],
+  },
+];
 
 const department = [
-    {
-        type: 'input',
-        message: 'What is the name of the department?',
-        name: 'name',
-    }   
-]
+  {
+    type: "input",
+    message: "What is the name of the department?",
+    name: "name",
+  },
+];
 
 const role = [
-    {
-        type: 'input',
-        message: 'What is the name of the role?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is the salary of the role?',
-        name: 'salary',
-    },
-    {
-        type: 'list',
-        message: 'In which department does this role belong to?',
-        name: 'department',
-        choices: ['Engineering', 'Finance', 'Legal', 'Sales']
-    }
-]
+  {
+    type: "input",
+    message: "What is the name of the role?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the salary of the role?",
+    name: "salary",
+  },
+  {
+    type: "list",
+    message: "In which department does this role belong to?",
+    name: "department",
+    choices: ["Engineering", "Finance", "Legal", "Sales"],
+  },
+];
 
 const employee = [
-    {
-        type: 'input',
-        message: 'First name of employee?',
-        name: 'first name',
-    },
-    {
-        type: 'input',
-        message: 'Last name of employee?',
-        name: 'last name',
-    },
-    {
-        type: 'list',
-        message: 'Employee Role?',
-        name: 'role',
-        choices: ['Sales Lead', 'Sales Person', 'Lead Engineer', 
-        'Software Engineer', 'Account Manager', 'Accountant', 
-        'Legal Team Lead', 'Lawyer']
-    },
-    {
-        type: 'list',
-        message: 'Who is the Manager of employee?',
-        name: 'manager',
-        choices: ['None', 'Christopher Wallace', 'Nasir Jones', 
-        'Lauryn Hill', 'Calvin Broadus']
-    }
-]
+  {
+    type: "input",
+    message: "First name of employee?",
+    name: "first",
+  },
+  {
+    type: "input",
+    message: "Last name of employee?",
+    name: "last",
+  },
+  {
+    type: "list",
+    message: "Employee Role?",
+    name: "role",
+    choices: [
+      "Sales Lead",
+      "Sales Person",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+      "Lawyer",
+    ],
+  },
+  {
+    type: "list",
+    message: "Who is the Manager of employee?",
+    name: "manager",
+    choices: [
+      "None",
+      "Christopher Wallace",
+      "Nasir Jones",
+      "Lauryn Hill",
+      "Calvin Broadus",
+    ],
+  },
+];
+
+const update = [
+  {
+    type: "list",
+    message: "Which employee role do you want to update?",
+    name: "employee",
+    choices: [
+      "Christopher Wallace",
+      "Tupac Shakur",
+      "Nasir Jones",
+      "Melissa  Elliott",
+      "Lauryn Hill",
+      "Onika Maraj",
+      "Calvin Broadus",
+      "Shawn Carter",
+    ],
+  },
+  {
+    type: "list",
+    message: "Which role do you want to assign to the employee?",
+    name: "role",
+    choices: [
+      "Sales Lead",
+      "Sales Person",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+      "Lawyer",
+    ],
+  },
+];
 
 // Menu prompt
 const showMenu = () => {
-    inquirer
-        .prompt(menu)
-        .then((answers) => {
-            console.log(answers);
-            switch (answers.menu) {
-                case 'View All Departments':
-                    viewAllDepartments();
-                    break;
-                case 'View All Roles':
-                    viewAllRoles();
-                    break;
-                case 'View All Employees':
-                    viewAllEmployees();
-                    break;
-                case 'Add a Department':
-                    addDepartment();
-                    break;
-                case 'Add a Role':
-                    addRole();
-                    break;
-                case 'Add an Employee':
-                    addEmployee();
-                    break;
-                case 'Update an Employee':
-                    updateEmployee();
-                    break; 
-                case 'Done':
-                    console.log('>>>>>> DONE <<<<<<')
-            }
-
-        })
-}
+  inquirer.prompt(menu).then((answers) => {
+    console.log(answers);
+    switch (answers.menu) {
+      case "View All Departments":
+        viewAllDepartments();
+        break;
+      case "View All Roles":
+        viewAllRoles();
+        break;
+      case "View All Employees":
+        viewAllEmployees();
+        break;
+      case "Add a Department":
+        addDepartment();
+        break;
+      case "Add a Role":
+        addRole();
+        break;
+      case "Add an Employee":
+        addEmployee();
+        break;
+      case "Update an Employee Role":
+        updateEmployee();
+        break;
+      case "Done":
+        console.log(">>>>>> DONE <<<<<<");
+    }
+  });
+};
 
 const viewAllDepartments = () => {
-    db.query('Select * From department', (err, res) => {
-        if (err) {
-            console.log(err);    
-        } else {
-            console.table(res);
-            showMenu();
-        }; 
-    });
-}
+  db.query("Select * From department", (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table(res);
+      showMenu();
+    }
+  });
+};
 
 const viewAllRoles = () => {
-    db.query('Select * From role', (err, res) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.table(res);
-            showMenu();
-        }; 
-    });
-}
+  db.query("Select * From role", (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table(res);
+      showMenu();
+    }
+  });
+};
 
 const viewAllEmployees = () => {
-    db.query('Select * From employee', function (err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.table(res);
-            showMenu();
-        };  
-    });
-}
+  db.query("Select * From employee", function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table(res);
+      showMenu();
+    }
+  });
+};
 
 const addDepartment = () => {
-    inquirer
-        .prompt(department)
-        .then((answers) => {
-            const sql = `Insert Into department(name) Values(?)`;
-            const params = [answers.name];
+  inquirer.prompt(department).then((answers) => {
+    const sql = `Insert Into department(name) Values(?)`;
+    const params = [answers.name];
 
-            db.query(sql, params, (err, res) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Department Added!');
-                }
-            })
-            showMenu();
-        })
-}
+    db.query(sql, params, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Department Added!");
+      }
+    });
+    showMenu();
+  });
+};
 
 const addRole = () => {
-    inquirer
-        .prompt(role)
-        .then((answers) => {
-            
-            const sql = `Insert Into role (title, salary, department_id ) VALUES (${answers.title}, ${answers.salary}, ${answers.department})`;
-            const params = [answers.title, answers.salary, answers.department];
+  inquirer.prompt(role).then((answers) => {
+    const sql = `SELECT id FROM department WHERE name = ?`;
+    const params = [answers.department];
 
-            db.query(sql, params, (err, res) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Role Added!");
-                }
-            })
-            // db.query('Insert Into role Set ?', 
-            //     {
-            //         title: answers.title,
-            //         salary: answers.salary,
-            //         department_id: answers.department
-            //     },
-            // (err, res) => {
-            //     if (err) {
-            //         console.log(err);
-            //     } else {
-            //         console.log('Role Added!');
-            //     }
-            // })
-            showMenu();
-        })
-}
+    db.query(sql, params, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const sql1 = `Insert Into role (title, salary, department_id ) VALUES (?, ?, ?)`;
+        const params = [answers.title, answers.salary, res[0].id];
+
+        db.query(sql1, params, (err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Role Added!");
+          }
+        });
+      }
+    });
+    showMenu();
+  });
+};
 
 const addEmployee = () => {
-    inquirer
-        .prompt(employee)
-        .then((answers) => {
-            console.log(answers);
-            // showMenu();
-        })
-}
+  inquirer.prompt(employee).then((answers) => {
+    const sql = `SELECT id FROM employee WHERE first_name = ? and last_name = ?`;
+    const arrayOfNames = answers.manager.split(" ");
+    const params = [arrayOfNames[0], arrayOfNames[1]];
+
+    db.query(sql, params, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("This is manager id", res);
+        let managerId = res[0].id;
+
+        const sql1 = `SELECT id FROM role WHERE title = ?`;
+        const params = [answers.role];
+
+        db.query(sql1, params, (err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("This is role id", res);
+            let roleId = res[0].id;
+            console.log("Success!");
+
+            const sql2 = `Insert Into employee (first_name, last_name, role_id, manager_id ) VALUES (?, ?, ?, ?)`;
+            const params = [answers.first, answers.last, roleId, managerId];
+
+            db.query(sql2, params, (err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Employee Added!");
+              }
+            });
+          }
+        });
+      }
+    });
+
+    showMenu();
+  });
+};
 
 const updateEmployee = () => {
-    console.table(employeeTable);
-    addEmployee();
-}
+  inquirer.prompt(update).then((answers) => {
+    console.log(answers)
+    const sql = `SELECT first_name, last_name FROM employee`;
+    const arrayOfNames = answers.employee.split(" ");
+    const params = [arrayOfNames[0], arrayOfNames[1]];
+
+    db.query(sql, params, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("This is employee", res);
+        let employeeId = res[0].id;
+
+        const sql1 = `SELECT id FROM role WHERE title = ?`;
+        const params = [answers.role];
+
+        db.query(sql1, params, (err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("This is role id", res);
+            let roleId = res[0].id;
+            console.log("Success!");
+
+            const sql2 = `UPDATE employee SET role_id = ? WHERE id = ?`;
+            const params = [roleId, employeeId];
+
+            db.query(sql2, params, (err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Employee Updated!");
+              }
+            });
+          }
+        });
+      }
+    });
+    showMenu();
+  });
+};
 
 showMenu();
-
